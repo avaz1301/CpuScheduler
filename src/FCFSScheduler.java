4beef1e58ddcfdc9686b98d7a52c89216876dd44
Angelo Zamudio <avaz1301@gmail.com>
2016-03-30 15:48:37 -0400

    Little update the broke everything

--- src/FCFSScheduler.java
+++ src/FCFSScheduler.java
@@ -1,43 +1,58 @@
 /**
- * Created by AngeloZ on 3/16/16.
+ * Created by Angelo Zamudio, Yat Sze So
  */
+import java.util.LinkedList;
+
 public class FCFSScheduler extends Scheduler {
 
-    public FCFSScheduler(){
-        super();
+    /**
+     * FCFSScheduler is created calling the super constructor
+     * from class Scheduler.
+     * @param q is the JobQ passed in from main class.
+     * @param n_jobs is the number of jobs in the JobQ.
+     */
+    public FCFSScheduler(LinkedList<PCB> q, int n_jobs){
+        super(q, n_jobs);
     }//constructor
 
     public void start_Burst(){//removed PCB x
-        if(ReadyQueue.isEmpty() && !BlockedQueue.isEmpty()){
+        if(ReadyQueue.isEmpty() && !BlockedQueue.isEmpty()) {
             updateTimer();
-            ioBurst();
         }else {
             PCB x=ReadyQueue.pop();
+            System.out.println("PCD id #: "+x.jobID);
+            System.out.println("READY SIZE: "+ReadyQueue.size());
             x.wait_time+=(globalTimer-x.ready_arrival);
             int i=0;
-            if(x.currentBurst < x.bursts.length/*x.burstCount*/){
+            if(x.currentBurst < x.bursts.length){
                 x.setBurstTime(x.currentBurst);
                 System.out.println("PCD id #: "+x.jobID+" is in CPU...");
-                while(i < x.curr_cpu_burst){
+                while(i < x.curr_cpu_burst ){
+
                     updateTimer();
-                    ioBurst();
                     i++;
+                    //ioBurst();
+
                 }//while
-                System.out.println("PCD id #: "+x.jobID+" finished CPU BURST, entering I/0...");
-                BlockedQueue.add(x);
-            }else{
-                completionExec(x);
-            }//else
+                //System.out.println("PCD id #: "+x.jobID+" finished CPU BURST, entering I/0....."+globalTimer);
+                if(x.currentBurst == x.bursts.length-1){
+                    completionExec(x);
+                    System.out.println("JOBS COMPLETED: "+ jobs_completed);
+                }else{
+                    BlockedQueue.add(x);
+                }//else
+
+            }//if
         }//else
     }//start_burst
 
     public void complete(){
         System.out.println("=================Scheduling End Report=====================");
         System.out.println("Scheduling Algorithm: FCFS");
-        System.out.println("Cpu Clock Value: "+globalTimer);
-        System.out.println("Average Processing time: "+ (s_processing_time / 526));
-        System.out.println("Average Wait Time: "+ (s_waiting_time / 526));
-        System.out.println("Average Turnaround Time: "+ (s_turnaround_time / 526));
+        System.out.println("Cpu Clock Value: "+(globalTimer-1));
+        System.out.println("Average Processing time: "+ (s_processing_time / jobs));
+        System.out.println("Average Wait Time: "+ (s_waiting_time / jobs));
+        System.out.println("Average Turnaround Time: "+ (s_turnaround_time / jobs));
         System.out.println("Total jobs Completed: "+jobs_completed);
         System.out.println("=================Scheduling End Report=====================");
     }//complete
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,8 +1,7 @@
-import java.util.*;
-
 /**
- * Created by AngeloZ on 3/16/16.
+ * Created by Angelo Zamudio, Yat Sze So
  */
+import java.util.*;
 
 public class Scheduler {
 	public int globalTimer = 0;
@@ -10,21 +9,43 @@ public class Scheduler {
 	public LinkedList<PCB> JobQueue;
 	public LinkedList<PCB> BlockedQueue;
 	public int readyQ_Max      = 0;
-	public int jobs_completed  = 0;
+	public int jobs = 0, jobs_completed  = 0;
     public int blocktime=10;
     public int s_processing_time = 0, s_waiting_time = 0, s_turnaround_time = 0;
     public int quantum;
 
-	public Scheduler(){
+
+    /**
+     * This is the constructor for the super calss Scheduler from which
+     * each of the 3 distinct schedulers will inherit their methods from.
+     *
+     * @param x This is the JobQ passed into the constructor from the main
+     *          when the Scheduler is instantiated.
+     * @param n_jobs int variable represents amount of jobs in JobQ.
+     */
+	public Scheduler(LinkedList<PCB> x, int n_jobs){
+        JobQueue=x;
 		ReadyQueue = new LinkedList<PCB>();
-		JobQueue=new LinkedList<PCB>();
 		BlockedQueue=new LinkedList<PCB>();
+        jobs=n_jobs;
+        //the following while fills the ReadyQueue witht he first 10
+        //jobs in the JobQueue.
+        while (readyQ_Max < 10 && !JobQueue.isEmpty()){
+            addtoReady();
+        }
+        //this loop starts the cpu simulation and continues until all
+        //jobs that were initially in job JobQueue have executed completely.
+        while(jobs_completed != jobs){
+            start_Burst();
+        }
+        //print summary of scheduling algorithm.
+        complete();
 	}//constructor
-	
-	public void fillJobQueue(PCB x){
-        JobQueue.add(x);
-	}//fillJobQueue
-	
+
+    /**
+     * Method which checks if there are still jobs present in JobQueue
+     * before adding the head PCB of JobQueue to ReadyQueue.
+     */
 	public void addtoReady(){
         if(!JobQueue.isEmpty()){
             JobQueue.peek().ready_arrival=globalTimer;
@@ -33,83 +54,88 @@ public class Scheduler {
         }//if
 	}//addtoReady
 
-	public void ioBurst() {//removed PCB x
+    /**
+     * method to simulate cpu processing. To be overridden in inheriting
+     * classes.
+     */
+    public void start_Burst(){};
+
+    /**
+     * This method simulates a PCB object in Blocked/IO Queue where they
+     * will wait for 10 time units as specified and be returned to Ready
+     * Queue if there are more remaining bursts.
+     */
+	public void ioBurst() {
+        //first check if there is PCB in BlockedQueue.If Blocked Queue is
+        //empty do nothing.
         if (!BlockedQueue.isEmpty()){
             PCB x = BlockedQueue.peek();
-            if (blocktime == 1) {
-                System.out.println("PCB id #: " + x.jobID + " finished i/o....");
-                System.out.println("PCB id #: " + x.jobID + " returning to ReadyQ....");
+            //If time in BlockQueue is 10 PCB is popped from Blocked
+            //Queue and added to the end of ReadyQueue.Else cycle one
+            //more time unit.
+            if (blocktime == 0) {
                 BlockedQueue.peek().ready_arrival = globalTimer;
                 blocktime=10;
                 ReadyQueue.add(BlockedQueue.remove());
             }else{
                blocktime=(blocktime-1);
-            }
-        }//if Blocked
+            }//else
+        }//if
 	}//burst
 
+    /**
+     * This is the method which increments globalTimer by one for every simulated
+     * time unit of CPU processing. Every 200 time units a summary is printed. This
+     * class also drives/checks Blocked Queue by calling ioBurst().
+     */
 	public void updateTimer(){
+        //If time is a multiple of 200 print out update summary.
 		if(globalTimer%200==0){
             System.out.println();
             System.out.println("=================TIMER UPDATE=====================");
 			System.out.println("Global Timer = "+globalTimer);
 			System.out.println("Ready Queue Size = "+ ReadyQueue.size());
+            //if block queue empty at time of summary print 0 else print BlockQueue size
             if(!BlockedQueue.isEmpty()){
 				System.out.println("Blocked Queue Size = "+ BlockedQueue.size());
 			}else{
 				System.out.println("Blocked Queue Size = 0");
-			}
+			}//else
             System.out.println("Jobs Completed: "+ jobs_completed);
             System.out.println("=================TIMER UPDATE=====================");
             System.out.println();
+            //check BlockedQueue
+            ioBurst();
+            //increment timer
             globalTimer++;
 		}else{
-           //System.out.println(globalTimer++);
+            ioBurst();
             globalTimer++;
 		}//else
 	}//updateTimer
 
+    /**
+     * Method will print out completed execution summary for respective scheduling
+     * algorithm. To be overridden in inheriting classes.
+     */
+    public void complete(){};
+
+    /**
+     * This Method takes in the completely executed PCB object recording all necessary
+     * time information. and adding the PCB processing/waiting/turnaround time to the
+     * global total
+     * @param x Is te PCB that has completed execution and will not be reentering
+     *          the system
+     */
     public void completionExec(PCB x){
-        System.out.println("PCB id #: " +x.jobID +" HAS EXECUTED COMPLETELY");
         x.completion_time=globalTimer;
+        //print necessary PCB information
         x.printPCBCompleted();
         s_processing_time += x.processing_time;
         s_waiting_time += x.wait_time;
         s_turnaround_time += x.turnaround_time;
-        jobs_completed++;
+        jobs_completed=jobs_completed+1;
         readyQ_Max-=1;
         addtoReady();
-    }
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-	public void printJob() {
-        while (JobQueue.peek() != null) {
-            JobQueue.remove().printPCB();
-        }
-    }
-	public void printRQueue(){
-		LinkedList<PCB> temp= ReadyQueue;
-		if(temp.peek()!=null){
-			System.out.println("----------readyQ---------------");
-			while(temp.peek()!=null){
-				temp.remove().printPCB();
-			}
-			System.out.println("----------readyQ---------------");
-		}//while
-	}//PrintQueue
-
-    public void printBQueue(){
-        LinkedList<PCB> temp= BlockedQueue;
-        if(temp.peek()!=null){
-            System.out.println("----------BlockedQ---------------");
-            while(temp.peek()!=null){
-                temp.remove().printPCB();
-            }
-            System.out.println("----------BlockedQ---------------");
-        }//while
-    }//PrintQueue
-	
+    }//completionExec
 }//LTScheduler
